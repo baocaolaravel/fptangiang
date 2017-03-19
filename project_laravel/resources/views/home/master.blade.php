4820d9740b9b80979d29282fc09974e7e57db806
baocaolaravel <quangtran0826@gmail.com>
2017-03-19 21:41:48 +0700

    ver.0.1

---
+++ project_laravel/resources/views/home/master.blade.php
@@ -0,0 +1,279 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>FPT Telecom - An Giang</title>
+
+    <!-- Bootstrap Core CSS -->
+    <link href="{!! url('public/home/css/bootstrap.css') !!}" rel="stylesheet">
+    <link href="{!! url('public/home/css/shop-homepage.css') !!}" rel="stylesheet">
+    <link href="{!! url('public/home/css/3-col-portfolio.css') !!}" rel="stylesheet">
+    <!-- Custom CSS -->
+    <link href="{!! url('public/home/css/scrolling-nav.css') !!}" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Fugaz+One" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Cormorant+Infant|Fugaz+One|Oswald" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+<style type="text/css">
+    .navbar-brand page-scroll:hover{
+        color: #111;
+    }
+</style>
+</head>
+
+<!-- The #page-top ID is part of the scrolling feature - the data-spy and data-target are part of the built-in Bootstrap scrollspy function -->
+
+<body id="page-top" data-spy="scroll" data-target=".navbar-fixed-top">
+
+    <!-- Navigation -->
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation" style="background: #FF4500;">
+        <div class="container">
+            <div class="navbar-header page-scroll">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand page-scroll" href="#page-top" style="font-family: 'Fugaz One', cursive;">FPT TELECOM - AN GIANG</a>
+            </div>
+
+            <!-- Collect the nav links, forms, and other content for toggling -->
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li class="hidden">
+                        <a class="page-scroll" href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#about" style="font-family: 'Oswald', sans-serif; font-size: 15pt;">DỊCH VỤ</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#services" style="font-family: 'Oswald', sans-serif; font-size: 15pt;">TIN TỨC</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact" style="font-family: 'Oswald', sans-serif; font-size: 15pt;" >LIÊN HỆ</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+        </div>
+        <!-- /.container -->
+    </nav>
+
+    <div class="col-md-10 col-md-offset-1">
+       <div class="row carousel-holder">
+          <div class="col-md-12">
+             <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                   <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+                   <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+                   <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                   <div class="item active">
+                      <img class="slide-image" src="http://placehold.it/800x300" alt="">
+                   </div>
+                   <div class="item">
+                      <img class="slide-image" src="http://placehold.it/800x300" alt="">
+                   </div>
+                   <div class="item">
+                      <img class="slide-image" src="http://placehold.it/800x300" alt="">
+                   </div>
+                </div>
+                <a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">
+                <span class="glyphicon glyphicon-chevron-left"></span>
+                </a>
+                <a class="right carousel-control" href="#carousel-example-generic" data-slide="next">
+                <span class="glyphicon glyphicon-chevron-right"></span>
+                </a>
+             </div>
+          </div>
+       </div>
+    </div>
+
+    <!-- Intro Section -->
+    <section id="intro" class="intro-section">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="row">
+                <div class="col-md-10">
+                
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- About Section -->
+    <section id="about" class="about-section">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>About Section</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Services Section -->
+    <section id="services" class="services-section" style="height: 980px;">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="row">
+                <div class="col-md-12">
+
+                        <!-- Page Header -->
+                        <div class="row">
+                            <div class="col-lg-12">
+                                <h1 class="page-header" style="font-family: 'Oswald', sans-serif;">TIN TỨC 24h
+                                </h1>
+                            </div>
+                        </div>
+                        <!-- /.row -->
+
+                        <!-- Projects Row -->
+                        <div class="row">
+                            <div class="col-md-4 portfolio-item">
+                                <a href="#">
+                                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
+                                </a>
+                                <h3>
+                                    <a href="#">Project Name</a>
+                                </h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+                            </div>
+                            <div class="col-md-4 portfolio-item">
+                                <a href="#">
+                                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
+                                </a>
+                                <h3>
+                                    <a href="#">Project Name</a>
+                                </h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+                            </div>
+                            <div class="col-md-4 portfolio-item">
+                                <a href="#">
+                                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
+                                </a>
+                                <h3>
+                                    <a href="#">Project Name</a>
+                                </h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+                            </div>
+                        </div>
+                        <!-- /.row -->
+
+                        <!-- Projects Row -->
+                        <div class="row">
+                            <div class="col-md-4 portfolio-item">
+                                <a href="#">
+                                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
+                                </a>
+                                <h3>
+                                    <a href="#">Project Name</a>
+                                </h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+                            </div>
+                            <div class="col-md-4 portfolio-item">
+                                <a href="#">
+                                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
+                                </a>
+                                <h3>
+                                    <a href="#">Project Name</a>
+                                </h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+                            </div>
+                            <div class="col-md-4 portfolio-item">
+                                <a href="#">
+                                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
+                                </a>
+                                <h3>
+                                    <a href="#">Project Name</a>
+                                </h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+                            </div>
+                        </div>
+                        <!-- /.row -->
+                        <hr>
+                        <!-- Pagination -->
+                        <div class="row text-center" style="margin-top: -10px;">
+                            <div class="col-lg-12">
+                                <ul class="pagination">
+                                    <li>
+                                        <a href="#">&laquo;</a>
+                                    </li>
+                                    <li class="active">
+                                        <a href="#">1</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">2</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">3</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">4</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">5</a>
+                                    </li>
+                                    <li>
+                                        <a href="#">&raquo;</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <!-- /.row -->
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Contact Section -->
+    <section id="contact" class="contact-section">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1>Contact Section</h1>
+                </div>
+            </div>
+        </div>
+    </section>
+    <hr>
+    <!-- Footer -->
+        <footer>
+            <div class="col-md-10 col-md-offset-1">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <p>Copyright &copy; Your Website 2014</p>
+                    </div>
+                </div>
+            </div>
+            <!-- /.row -->
+        </footer>
+    
+    <!-- jQuery -->
+    <script src="{!! url('public/home/js/jquery.js') !!}"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="{!! url('public/home/js/bootstrap.min.js') !!}"></script>
+
+    <!-- Scrolling Nav JavaScript -->
+    <script src="{!! url('public/home/js/jquery.easing.min.js') !!}"></script>
+    <script src="{!! url('public/home/js/scrolling-nav.js') !!}"></script>
+    
+
+</body>
+
+</html>
